60d5837d56940675bd3718ea67782c91f14cd8e7
Divya Patel <162137928+Divya04Patel@users.noreply.github.com>
2025-07-26 11:42:45 +0530

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,12 @@
+public class Customer {
+    private String customerId;
+    private final String name;
+
+    public Customer(String customerId, String name) {
+        this.customerId = customerId;
+        this.name = name;
+    }
+
+    public String getCustomerId() { return customerId; }
+    public String getName() { return name; }
+}
